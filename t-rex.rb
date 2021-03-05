3db39090b90cf562026e8ecd5270c41c6b622876
Geir Isene <g@isene.com>
2021-03-05 01:18:27 +0100

    Updated to latest version of curses class extention

--- t-rex.rb
+++ t-rex.rb
@@ -7,27 +7,65 @@ include  Curses
 
 # CLASSES
 class Curses::Window # CLASS EXTENSION 
-  attr_accessor :color, :attr
   # General extensions (see https://github.com/isene/Ruby-Curses-Class-Extension)
-  def clr
+  attr_accessor :color, :fg, :bg, :attr
+  # Set self.color for an already defined color pair such as: init_pair(1, 255, 3)
+  # The color pair is defined like this: init_pair(index, foreground, background)
+  # self.fg is set for the foreground color (and is used if self.color is not set)
+  # self.bg is set for the background color (and is used if self.color is not set)
+  # self.attr is set for text attributes like Curses::A_BOLD
+  def clr # Clears the whole window
     self.setpos(0, 0)
     self.maxy.times {self.deleteln()}
     self.refresh
     self.setpos(0, 0)
   end
-  def fill # Fill window with color as set by :bg
+  def fill # Fill window with color as set by self.color (or self.bg if not set) 
+    self.setpos(0, 0)
+    self.fill_from_cur_pos
+  end
+  def fill_from_cur_pos # Fills the rest of the window from current line
+    x = curx
+    y = cury
+    self.setpos(y, 0)
+    blank = " " * self.maxx
+    if self.color == nil
+      self.bg = 0 if self.bg   == nil
+      self.fg = 255 if self.fg == nil
+      init_pair(self.fg, self.fg, self.bg)
+      self.maxy.times {self.attron(color_pair(self.fg)) {self << blank}}
+    else
+      self.maxy.times {self.attron(color_pair(self.color)) {self << blank}}
+    end
+    self.refresh
+    self.setpos(y, x)
+  end
+  def fill_to_cur_pos # Fills the window up to current line
+    x = curx
+    y = cury
     self.setpos(0, 0)
     blank = " " * self.maxx
-    self.maxy.times {self.attron(color_pair(self.color)) {self << blank}}
+    if self.color == nil
+      self.bg = 0 if self.bg   == nil
+      self.fg = 255 if self.fg == nil
+      init_pair(self.fg, self.fg, self.bg)
+      y.times {self.attron(color_pair(self.fg)) {self << blank}}
+    else
+      y.times {self.attron(color_pair(self.color)) {self << blank}}
+    end
     self.refresh
-    self.setpos(0, 0)
+    self.setpos(y, x)
   end
-  def p(text)
-    self.attron(color_pair(self.color)) { self << text }
-    self.refresh
-  end
-  def pa(text)
-    self.attron(color_pair(self.color) | self.attr) { self << text }
+  def p(text) # Puts text to window
+    self.attr = 0 if self.attr == nil
+    if self.color == nil
+      self.bg = 0 if self.bg   == nil
+      self.fg = 255 if self.fg == nil
+      init_pair(self.fg, self.fg, self.bg)
+      self.attron(color_pair(self.fg) | self.attr) { self << text }
+    else
+      self.attron(color_pair(self.color) | self.attr) { self << text }
+    end
     self.refresh
   end
 end
@@ -670,7 +708,7 @@ def pstack
   @w_yzt.setpos(0,0)
   @w_yzt.p(t.rjust(30) + z.rjust(30) + y.rjust(30))
   @w_x.setpos(0,0)
-  @w_x.pa(x.rjust(30))
+  @w_x.p(x.rjust(30))
 end
 def pregs
   @w_reg.setpos(1,0)
@@ -708,7 +746,7 @@ CMDTEXT
           @w_cmd.p(c)
         else
           @w_cmd.color = 10
-          @w_cmd.pa(c)
+          @w_cmd.p(c)
         end
       end
       @w_cmd.p("\n")
